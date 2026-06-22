3fdd9f944ddceb2a6577ce8c77b7d1323f91576f
tray <at-box@mail.ru>
2026-06-22 13:51:58 +0500

    linted

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.contrib import messages
 
 
 # Create your views here.
--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
