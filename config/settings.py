1e67c247f1a838adcce635f6e0352c8e3dff6305
tray <at-box@mail.ru>
2026-07-06 09:49:37 +0500

    fix: DEBUG setting

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     "*",
