4bf5729080dd90f7e99a5e13f3add71e94430932
tray <at-box@mail.ru>
2026-07-06 16:29:56 +0500

    command updated

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -9,6 +9,7 @@ from typing import Any
 from django.core.management import BaseCommand, CommandError, call_command
 
 from catalog.models import Category, Contact, Feedback, Product
+from blog.models import BlogPost
 
 
 class Command(BaseCommand):
@@ -20,12 +21,19 @@ class Command(BaseCommand):
         Category.objects.all().delete()
         Contact.objects.all().delete()
         Feedback.objects.all().delete()
+        BlogPost.objects.all().delete()
 
         try:
             call_command("loaddata", "catalog_fixture.json", app="catalog", format="json", ignorenonexistent=True)
-            self.stdout.write(self.style.SUCCESS("Successfully loaded data from fixture"))
+            self.stdout.write(self.style.SUCCESS("Successfully loaded catalog data from fixture"))
         except CommandError:
-            self.stdout.write(self.style.ERROR("Failed to load data from fixture"))
+            self.stdout.write(self.style.ERROR("Failed to load catalog data from fixture"))
+
+        try:
+            call_command("loaddata", "blog_fixture.json", app="blog", format="json", ignorenonexistent=True)
+            self.stdout.write(self.style.SUCCESS("Successfully loaded blog data from fixture"))
+        except CommandError:
+            self.stdout.write(self.style.ERROR("Failed to load blog data from fixture"))
 
         cat1, _ = Category.objects.get_or_create(category_name="Cat1", category_description="Cat1")
 
@@ -37,6 +45,6 @@ class Command(BaseCommand):
         for prod in products:
             _, created = Product.objects.get_or_create(**prod)
             if created:
-                self.stdout.write(self.style.SUCCESS(f"Successfully created book: {prod["product_name"]}"))
+                self.stdout.write(self.style.SUCCESS(f"Successfully created product: {prod["product_name"]}"))
             else:
-                self.stdout.write(self.style.WARNING(f"Book already exists: {prod["product_name"]}."))
+                self.stdout.write(self.style.WARNING(f"Product already exists: {prod["product_name"]}."))
