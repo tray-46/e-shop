fb89444ace54e3ceb00537da66183e039fd6ce40
tray <at-box@mail.ru>
2026-07-20 21:36:24 +0500

    UserAdmin added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from users.models import User
+
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """User admin model"""
+
+    list_display = (
+        "id",
+        "email",
+    )
+    search_fields = ("email",)
