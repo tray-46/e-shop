17b13af7c49066c29884a404af618cf2e717e488
tray <at-box@mail.ru>
2026-07-20 18:49:33 +0500

    RegisterView added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from users.forms import RegisterForm
 
 # Create your views here.
+class RegisterView(CreateView):
+    template_name = "users/register.html"
+    form_class = RegisterForm
+    success_url = reverse_lazy("users:login")
