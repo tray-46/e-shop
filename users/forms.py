3297c6c9db454d8e4b50c083cce59080b4bc8cf1
tray <at-box@mail.ru>
2026-07-20 18:49:12 +0500

    RegisterForm added

---
+++ users/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from users.models import User
+
+
+class RegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control",})
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("email",)
